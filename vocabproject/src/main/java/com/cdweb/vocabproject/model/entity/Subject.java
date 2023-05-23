aa8e09b3dc3945b9af30a89498d3ef4373754a89
ntnhuy121 <19130272@st.hcmuaf.edu.vn>
2023-05-23 09:54:32 +0700

    subject model

--- vocabproject/src/main/java/com/cdweb/vocabproject/model/entity/Subject.java
+++ vocabproject/src/main/java/com/cdweb/vocabproject/model/entity/Subject.java
@@ -11,6 +11,18 @@ import java.io.Serializable;
 @Entity
 @Table(name = "subject")
 public class Subject extends BaseEntity implements Serializable {
+    @Column(name = "id")
+    private long id;
+    @Column(name = "tittle")
+    private String tittle ;
+    @Column(name = "description")
+    private String description ;
+    @Column(name = "status")
+    private boolean status ;
+    @OneToOne(cascade = CascadeType.ALL,fetch = FetchType.EAGER)
+    @JoinColumn(name = "account_id")
+    private Account account ;
+
 
     
 }
