6103518c22a759dfd348937dd2e30fdd8a017cab
lethuyvy12102608 <19130269@st.hcmuaf.edu.vn>
2023-06-04 19:34:15 +0700

    add flashcard

--- vocabproject/src/main/java/com/cdweb/vocabproject/controller/FlashcardController.java
+++ vocabproject/src/main/java/com/cdweb/vocabproject/controller/FlashcardController.java
@@ -1,5 +1,6 @@
 package com.cdweb.vocabproject.controller;
 
+import com.cdweb.vocabproject.model.dto.VocabularyDTO;
 import com.cdweb.vocabproject.model.entity.Subject;
 import com.cdweb.vocabproject.model.entity.Vocabulary;
 import com.cdweb.vocabproject.model.mapper.SubjectMapper;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -29,17 +31,22 @@ public class FlashcardController {
     @Autowired
     private SubjectService subjectService;
 
-    @Autowired
-    private SubjectMapper subjectMapper;
-
     @GetMapping("/{subjectId}")
     public String flashcardHome(Model model, @PathVariable long subjectId) {
         Subject subject = subjectService.findById(subjectId);
+        Vocabulary vocabulary = new Vocabulary();
         List<Vocabulary> vocabularies = vocabularyService.findBySubject(subject);
-        model.addAttribute("vocabulariesDTO", vocabularyMapper.toListDTO(vocabularies));
-        model.addAttribute("firstVocab", vocabularies.get(0));
-        vocabularies.remove(0);
-        model.addAttribute("vocabularies", vocabularies);
+        if (vocabularies.size() != 0) {
+            model.addAttribute("vocabulariesDTO", vocabularyMapper.toListDTO(vocabularies));
+            model.addAttribute("firstVocab", vocabularies.get(0));
+            vocabularies.remove(0);
+            model.addAttribute("vocabularyDTO", vocabulary);
+            model.addAttribute("vocabularies", vocabularies);
+            return "flashcard";
+        }
+        model.addAttribute("subjectId", subjectId);
+        model.addAttribute("vocabularyDTO", vocabulary);
+        model.addAttribute("vocabulariesDTO", null);
         return "flashcard";
     }
 
@@ -48,5 +55,14 @@ public class FlashcardController {
         return "edit-flashcard";
     }
 
+    @PostMapping("/{subjectId}")
+    public String addFlashcard(@PathVariable long subjectId, VocabularyDTO vocabularyDTO){
+        vocabularyDTO.setStatus(true);
+        vocabularyDTO.setSubjectId(subjectId);
+        Vocabulary vocabulary = vocabularyMapper.toEntity(vocabularyDTO);
+        vocabularyService.save(vocabulary);
+        String redirectUrl = "/flashcard/" + subjectId;
+        return "redirect:" + redirectUrl;
+    }
 
 }
