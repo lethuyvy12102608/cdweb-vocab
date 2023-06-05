14c89dd847b0942246a6b6465993f28f2767cf7f
ntnhuy121 <19130272@st.hcmuaf.edu.vn>
2023-06-05 15:25:43 +0700

    checkout

---
+++ vocabproject/src/main/java/com/cdweb/vocabproject/controller/LogoutController.java
@@ -0,0 +1,34 @@
+package com.cdweb.vocabproject.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping(value = {"/logout"})
+public class LogoutController {
+
+    @GetMapping(value = {""})
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            if (auth != null) {
+                new SecurityContextLogoutHandler().logout(request, response, auth);
+                request.getSession().invalidate();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        String redirectUrl = "/login";
+        return "redirect:" + redirectUrl;
+    }
+
+
+}
